9dd07efdfbb2628d086b0542832fea5510d8e0af
Влад Бочкарёв <ichi222@yandex.ru>
2022-04-26 17:51:20 +0300

    My second commit

--- homework.py
+++ homework.py
@@ -7,11 +7,13 @@ class InfoMessage:
         self.speed = speed
         self.calories = calories
     def get_message(self):
-        return f'Тип тренировки: {self.training_type: .3f}; Длительность: {self.duration: .3f} ч.; Дистанция: {self.distance: .3f} км.; Ср. скорость: {self.speed: .3f} км/ч; Потрачено ккал: {self.calories: .3f}.'
+        print (f'Тип тренировки: {self.training_type};' 'Длительность: {self.duration:.3f} ч.;' 'Дистанция: {self.distance:.3f} км.;' 'Ср. скорость: {self.speed:.3f} км/ч;' 'Потрачено ккал: {self.calories:.3f}.')
     pass
 
 class Training:
     """Базовый класс тренировки."""
+    LEN_STEP = 0.65
+    M_IN_KM = 1000
 
     def __init__(self,
                  action: int,
@@ -25,15 +27,13 @@ class Training:
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        M_IN_KM = 1000
-        LEN_STEP = 0.65
-        distance = self.action * LEN_STEP / M_IN_KM
+        distance = self.action * self.LEN_STEP / self.M_IN_KM
         return distance
         pass
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        mean_speed = self.get_distance / self.duration
+        mean_speed = self.get_distance() / self.duration
         return mean_speed
         pass
 
@@ -53,36 +53,42 @@ class Training:
 
 
 class Running(Training):
-    coeff_calorie_1 = 18
-    coeff_calorie_2 = 20
+    coeff_calories_running_1: int = 18
+    coeff_calories_running_2: int = 20
+    MINS_INTO_SECONDS = 60
     M_IN_KM = 1000
     """Тренировка: бег."""
     def __init__(self, action, duration, weight):
         super().__init__(action, duration, weight)
     def get_spent_calories(self):
-        spent_calories = (self.coeff_calorie_1 * self.get_mean_speed - self.coeff_calorie_2) * self.weight / self.M_IN_KM * self.duration
+        spent_calories = ((self.coeff_calories_running_1 * self.get_mean_speed()
+                 - self.coeff_calories_running_2) 
+                 * self.weight/self.M_IN_KM * self.duration* self.MINS_INTO_SECONDS)
         return spent_calories
     pass
 
 
 class SportsWalking(Training):        
-    coeff_calorie_1 = 0.035
-    coeff_calorie_2 = 2
-    coeff_calorie_3 = 0.029
+    coeff_calories_walking_1 = 0.035
+    coeff_calories_walking_2 = 2
+    coeff_calories_walking_3 = 0.029
     """Тренировка: спортивная ходьба."""
     def __init__(self, action, duration, weight, height):
         super().__init__(action, duration, weight)
         self.height = height
     def get_spent_calories(self):
-        spent_calories = (self.coeff_calorie_1 * self.weight + (self.get_mean_speed**self.coeff_calorie_2//self.height) * self.coeff_calorie_3 * self.weight) * self.duration
+        spent_calories = ((self.coeff_calories_walking_1 * self.weight 
+                + (self.get_mean_speed**self.coeff_calories_walking_2 // self.height) 
+                * self.coeff_calories_walking_3*self.weight) 
+                * self.duration*60)
         return spent_calories
 
     pass
 
 
 class Swimming(Training):
-    COEFF_1 = 1.1
-    COEFF_2 = 2
+    coeff_calorie_swimming_1: float = 1.1
+    coeff_calorie_swimming_2: int = 2
     M_IN_KM = 1000
     LEN_STEP = 1.38
     """Тренировка: плавание."""
@@ -91,10 +97,11 @@ class Swimming(Training):
         self.lenght_pool = lenght_pool
         self.count_pool = count_pool
     def get_mean_speed(self):
-        mean_speed = self.lenght_pool * self.count_pool / self.M_IN_KM / self.duration
+        mean_speed = (self.lenght_pool * self.count_pool / self.M_IN_KM / self.duration)
         return mean_speed
     def get_spent_calories(self):
-        spent_calories = (self.get_mean_speed + self.COEFF_1) * self.COEFF_2 * self.weight
+        spent_calories = ((self.get_mean_speed() + self.coeff_calorie_swimming_1) 
+                 * self.coeff_calorie_swimming_2  * self.weight)
         return spent_calories
     def get_distance(self):
         distance = self.action * self.LEN_STEP / self.M_IN_KM
